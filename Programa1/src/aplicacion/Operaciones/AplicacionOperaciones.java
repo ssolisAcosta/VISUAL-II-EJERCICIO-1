284cc5ae10e075f0f2e5265776bc0e657a8a2139
SSolis <SSolis@SSISTEMAS>
2017-10-18 08:45:37 -0500

    Programa 1 de POO II

---
+++ Programa1/src/aplicacion/Operaciones/AplicacionOperaciones.java
@@ -0,0 +1,18 @@
+package aplicacion.Operaciones;
+
+import operaciones.numeros.OperacionesBasicas;
+
+
+/**
+ *
+ * @author SSolis
+ */
+public class AplicacionOperaciones {
+    
+    public static void main(String[] args) {
+        OperacionesBasicas obj = new OperacionesBasicas();
+        System.out.println("La suma es:"+obj.suma(5,7));
+    }
+    
+    
+}
---
+++ Programa1/src/operaciones/numeros/OperacionesBasicas.java
@@ -0,0 +1,11 @@
+package operaciones.numeros;
+
+/**
+ *
+ * @author SSolis
+ */
+public class OperacionesBasicas {
+public int suma(int x, int y){
+    return x+y;
+}
+}
