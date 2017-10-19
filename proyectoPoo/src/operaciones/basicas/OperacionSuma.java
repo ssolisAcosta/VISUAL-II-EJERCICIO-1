6ecc5fc8c11dbe116aae6512d8ae2b645900c369
SSolis <SSolis@SSISTEMAS>
2017-10-19 08:48:45 -0500

    Programa 1 de POO II

---
+++ proyectoPoo/src/operaciones/basicas/OperacionSuma.java
@@ -0,0 +1,16 @@
+package operaciones.basicas;
+
+import java.util.Scanner;
+
+/**
+ * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class OperacionSuma {
+
+    public static void main(String[] args) {
+        System.out.println("holoa mundo");
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
