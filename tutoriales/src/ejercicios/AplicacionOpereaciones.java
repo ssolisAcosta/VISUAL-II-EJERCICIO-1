7ce6644e31bcd009be80bf9a7c2891fae485b28d
SSolis <SSolis@SSISTEMAS>
2017-10-19 22:40:21 -0500

    Se aumento JOptionPane

---
+++ tutoriales/src/ejercicios/AplicacionOpereaciones.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+/**
+ * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class AplicacionOpereaciones {
+
+    public static void main(String[] args) {
+        int x=2;
+        int y =3;
+       // x,y=y,x;
+        System.out.println((x+6)%y);
+        
+    }
+    
+            
+}
