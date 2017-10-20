2dff993c95149e7cab1cc6c5252c8575da2a1648
SSolis <SSolis@SSISTEMAS>
2017-10-20 09:01:43 -0500

    Se aumento JOptionPane

---
+++ estructuraDatos1/src/clases/basicas/OperacionesDeber.java
@@ -0,0 +1,17 @@
+package clases.basicas;
+
+/**
+ * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class OperacionesDeber {
+
+    public static void metodo1(){
+        boolean b = true;
+        int i=1;
+        if(b){
+            i++;
+        }
+        System.out.println(i);
+    }
+}
---
+++ estructuraDatos1/src/desarrollos/Aplicacion.java
@@ -0,0 +1,16 @@
+package desarrollos;
+import clases.basicas.OperacionesDeber;
+
+/**
+ * @Estructura de datos
+ * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        OperacionesDeber.metodo1();
+        
+    }
+           
+
+}
